da512bebffa98ce9eabe27d5dabd3fe0a58783d3
CJ302217X <CJ302217X@LAB01-PC15>
2024-03-06 09:08:54 -0300

    Adicionar arquivos de projeto.

---
+++ ATV3_CJ302217X/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ATV3_CJ302217X
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float n;
+
+            Console.WriteLine("Digite a sua nota");
+            n = float.Parse(Console.ReadLine());
+
+            if (n >= 6)
+            {
+                Console.WriteLine("Aluno foi aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Aluno foi Reprovado");
+            }
+        }
+    }
+}
